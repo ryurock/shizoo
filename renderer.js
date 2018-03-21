bea5d3e54af9dd7b8433cd63f8459959f3539318
ryurock <ryusukekimura3@gmail.com>
2018-03-21 12:43:00 +0900

    typescriptの導入を断念

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+window.jQuery = window.$ = require('jquery');
+require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
+require('./node_modules/popper.js/dist/umd/popper.min.js');
+
+const {ipcRenderer} = require('electron')
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg) // prints "pong"
+})
+ipcRenderer.send('asynchronous-message', 'ping')
