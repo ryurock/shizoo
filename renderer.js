5ee40020794d2bd70231b07d2d10b66d8c54f44c
ryurock <ryusukekimura3@gmail.com>
2018-03-21 19:36:50 +0900

    ディレクトリ構成を変更

--- renderer.js
+++ renderer.js
@@ -2,6 +2,12 @@ window.jQuery = window.$ = require('jquery');
 require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('./node_modules/popper.js/dist/umd/popper.min.js');
 
+
+
+
+
+
+
 const {ipcRenderer} = require('electron')
 console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
 
